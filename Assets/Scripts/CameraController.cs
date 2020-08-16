634700c9e8c8147fc87c6eca304ffd1d07b6b3f4
Dejan Trajkovic <dtrajko@gmail.com>
2020-08-17 01:17:45 +0200

    13. Making the camera follow

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = target.position;
+        transform.rotation = target.rotation;
+    }
+}
