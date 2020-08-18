6d3bcb7cc48c611b35e95b6369dfce8a88a57fc4
Veljko Trajkovic <veljkotrajkovic007@gmail.com>
2020-08-18 19:39:10 +0200

    22. Moving The Projectile

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+    public Rigidbody theRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = transform.forward * moveSpeed;
+    }
+}
