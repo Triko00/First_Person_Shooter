243365b0c3aa077f78e259e6ec25df4f34d7d615
Veljko Trajkovic <veljkotrajkovic007@gmail.com>
2020-08-19 16:10:32 +0200

    32. Moving The Enemy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody theRB; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(PlayerController.instance.transform.position);
+
+        theRB.velocity = transform.forward * moveSpeed;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public static PlayerController instance;
+    
     public float moveSpeed, gravityModifier, jumpPower, runSpeed;
     public CharacterController charCon;
 
@@ -23,6 +25,11 @@ public class PlayerController : MonoBehaviour
 
     public GameObject bullet;
     public Transform firePoint;
+
+    private void Awake()
+    {
+        instance = this;
+    }
 
     // Start is called before the first frame update
     void Start()
