64bd6580df769cf257445160e18c401f4b0957e3
Veljko Trajkovic <veljkotrajkovic007@gmail.com>
2020-08-19 14:49:29 +0200

    29. Destroying Targets

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -33,6 +33,11 @@ public class BulletController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+        }
+
         Destroy(gameObject);
         Instantiate(impactEffect, transform.position + (transform.forward * (-moveSpeed * Time.deltaTime)), transform.rotation);
     } 
