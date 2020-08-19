9de92820255f57632cef9d89d23bface40556412
Veljko Trajkovic <veljkotrajkovic007@gmail.com>
2020-08-19 15:22:24 +0200

    30. CHALLENGE - Create moving targets

---
+++ Assets/Scripts/TargetMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMovement : MonoBehaviour
+{
+    public bool shouldMove, shouldRotate;
+
+    public float moveSpeed, rotateSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(shouldMove)
+        {
+            transform.position += new Vector3(moveSpeed, 0f, 0f) * Time.deltaTime;
+        }
+
+        if(shouldRotate)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, rotateSpeed * Time.deltaTime, 0f));
+        }
+    }
+}
