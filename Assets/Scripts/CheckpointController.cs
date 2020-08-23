e0f0aae7dd261a1c1f8503a111eed81755cc604a
Veljko Trajkovic <veljkotrajkovic007@gmail.com>
2020-08-23 12:24:27 +0200

    48. Creating Checkpoints

---
+++ Assets/Scripts/CheckpointController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheckpointController : MonoBehaviour
+{
+    public string cpName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PlayerPrefs.HasKey(SceneManager.GetActiveScene().name + "_cp"))
+        {
+            if (PlayerPrefs.GetString(SceneManager.GetActiveScene().name + "_cp") == cpName)
+            {
+                PlayerController.instance.transform.position = transform.position;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            PlayerPrefs.SetString(SceneManager.GetActiveScene().name + "_cp", "");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            PlayerPrefs.SetString(SceneManager.GetActiveScene().name + "_cp", cpName);
+        }
+    }
+
+}
