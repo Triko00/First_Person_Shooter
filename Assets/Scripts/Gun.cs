1b1e3d71fd4bc2dd1bc0e4d644fed88d4d40d2a5
Veljko Trajkovic <veljkotrajkovic007@gmail.com>
2020-08-24 16:11:20 +0200

    56. Ammo Pickups

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -12,7 +12,7 @@ public class Gun : MonoBehaviour
     [HideInInspector]
     public float fireCounter;
 
-    public int currentAmmo;
+    public int currentAmmo, pickupAmount;
 
     // Start is called before the first frame update
     void Start()
@@ -27,5 +27,12 @@ public class Gun : MonoBehaviour
         {
             fireCounter -= Time.deltaTime;
         }
+    }
+
+    public void GetAmmo()
+    {
+        currentAmmo += pickupAmount;
+
+        UIController.instance.ammoText.text = "AMMO: " + currentAmmo;
     }
 }
---
+++ Assets/Scripts/Pickups/AmmoPickupController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickupController : MonoBehaviour
+{
+    private bool collected;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player" && !collected)
+        {
+            PlayerController.instance.activeGun.GetAmmo();
+
+            Destroy(gameObject);
+
+            collected = true;
+        }
+    }
+}
