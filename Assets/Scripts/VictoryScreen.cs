647f02f30634424c975363ab6bc71d6363d33a69
Veljko Trajkovic <veljkotrajkovic007@gmail.com>
2020-08-25 18:02:43 +0200

    69. CHALLENGE - Create a Victory Screen

---
+++ Assets/Scripts/VictoryScreen.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class VictoryScreen : MonoBehaviour
+{
+    public string mainMenuScene;
+
+    public float timeBetweenShowing;
+
+    public GameObject textBox, returnButton;
+
+    public Image blackScreen;
+    public float blackScreenFade;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ShowObjectsCo());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        blackScreen.color = new Color(blackScreen.color.r, blackScreen.color.g, blackScreen.color.b, Mathf.MoveTowards(blackScreen.color.a, 0f, blackScreenFade * Time.deltaTime));
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(mainMenuScene);
+    }
+
+    public IEnumerator ShowObjectsCo()
+    {
+        yield return new WaitForSeconds(timeBetweenShowing);
+
+        textBox.SetActive(true);
+
+        yield return new WaitForSeconds(timeBetweenShowing);
+
+        returnButton.SetActive(true);
+    }
+}
