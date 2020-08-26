e979565b4ae802f0a161e7932139ad00332b3f85
Veljko Trajkovic <veljkotrajkovic007@gmail.com>
2020-08-26 17:10:50 +0200

    77. Adding Rockets

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public int damage = 25;
+
+    public bool damageEnemy, damagePlayer;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy" && damageEnemy)
+        {
+            //Destroy(other.gameObject);
+            other.gameObject.GetComponent<EnemyHealthController>().DamageEnemy(damage);
+        }
+
+        if (other.gameObject.tag == "Player" && damagePlayer)
+        {
+            //Debug.Log("Hit Player at " + transform.position);
+            PlayerHealthController.instance.DamagePlayer(damage);
+        }
+    }
+}
